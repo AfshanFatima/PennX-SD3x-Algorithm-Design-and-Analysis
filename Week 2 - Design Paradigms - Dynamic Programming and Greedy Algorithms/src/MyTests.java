a9dad4d3bf841d37888e346a503d437f34dcaa56
Afshan Fatima <fafshan09@gmail.com>
2019-01-24 10:02:48 -0700

    MyTests.java

---
+++ Week 2 - Design Paradigms - Dynamic Programming and Greedy Algorithms/src/MyTests.java
@@ -0,0 +1,36 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class MyTests {
+	
+	/*TODO: Add your own test cases here!
+	 * We've provided a sample test case for each problem below
+	 * You can use these as building blocks to write your own test cases
+	 */
+	
+	@Test
+	public void HuffmanSampleTest() {
+		String input = "abc";
+		Huffman h = new Huffman(input);
+		String encoding = h.encode();
+		assertEquals(input, h.decode(encoding));
+		assertEquals("huffman abc compression", Huffman.compressionRatio(input), 0.20833, 0.01);
+	}
+	
+	@Test
+	public void IntervalSampleTest() {
+		GreedyDynamicAlgorithms.Interval red = new GreedyDynamicAlgorithms.Interval(1, 3);
+		GreedyDynamicAlgorithms.Interval blue = new GreedyDynamicAlgorithms.Interval(0, 4);
+		ArrayList<GreedyDynamicAlgorithms.Interval> reds = new ArrayList<>();
+		ArrayList<GreedyDynamicAlgorithms.Interval> blues = new ArrayList<>();
+		reds.add(red);
+		blues.add(blue);
+		int expectedOptimal = 1;
+		int actualOptimal = GreedyDynamicAlgorithms.optimalIntervals(reds, blues);
+		assertEquals("interval 1 red 1 blue", expectedOptimal, actualOptimal);
+	}
+
+}
