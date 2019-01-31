b21ef7deb8404661b4044a4f51edd832ed642490
Afshan Fatima <fafshan09@gmail.com>
2019-01-31 12:39:59 -0700

    Main.java

---
+++ Week 3 - Graph Algorithms - Traversals and Spannig Trees/src/Main.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Maze m = new Maze("files/mazeMedium.txt");
+		m.g.printGraph();
+	}
+}
